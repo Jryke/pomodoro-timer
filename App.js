a65423dd4f937d7d45b74f2228a32488423e3bd0
Jryke <rykerson@gmail.com>
2020-04-04 14:32:17 -0400

    basic UI complete

--- App.js
+++ App.js
@@ -1,14 +1,45 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+const App = () => {
+  const [workInterval, setWorkInterval] = useState();
+  const [breakInterval, setBreakInterval] = useState();
+
+  const startTimer = () => console.log('start timer clicked');
+
+  return (
+    <View style={styles.container}>
+      <Text>Current: working</Text>
+      <Text>Timer: 0</Text>
+      <View style={styles.inputContainer}>
+        <Text>Work Interval: </Text>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={number => setWorkInterval(number)}
+          placeholder='0'
+          defaultValue={workInterval}
+          keyboardType={'numeric'}
+        />
       </View>
-    );
-  }
+      <View style={styles.inputContainer}>
+        <Text>Break Interval: </Text>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={number => setBreakInterval(number)}
+          placeholder='0'
+          defaultValue={breakInterval}
+          keyboardType={'numeric'}
+        />
+      </View>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={startTimer}
+      >
+        <Text>Start Timer</Text>
+      </TouchableOpacity>
+
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +49,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  inputContainer: {
+    flexDirection: 'row'
+  },
+  textInput: {
+    height: 20,
+    width: 40,
+    borderColor: 'gray', 
+    borderWidth: 1
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10
+  }
 });
+
+export default App;
