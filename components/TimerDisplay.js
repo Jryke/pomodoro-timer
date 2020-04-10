fc2782759bc3523216c5191a9fff2e6b34f2d4b9
Jryke <rykerson@gmail.com>
2020-04-09 20:17:08 -0400

    component refactored, counter rendering properly

--- components/TimerDisplay.js
+++ components/TimerDisplay.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Text } from 'react-native';
 
-const renderCountdown = (counter) => {
-  if (counter.seconds < 10) {
-    if (counter.minutes) return `${counter.minutes}:0${counter.seconds}`
-    return `0:0${counter.seconds}`
+const renderCountdown = counter => {
+  if (counter.minutes != null) {
+    if (counter.seconds < 10) return `${counter.minutes}:0${counter.seconds}`
+    return `${counter.minutes}:${counter.seconds}`
   }
-  return `${counter.minutes}:${counter.seconds}`
-}
+  if (counter.seconds < 10) return `0:0${counter.seconds}`
+  return `0:${counter.seconds}`
+};
 
 const TimerDisplay = (props) => {
   return (
-    <Text>Timer: {props.counter.minutes || props.counter.seconds ? renderCountdown(props.counter) : null}</Text>
+    <Text>{props.counter.minutes || props.counter.seconds ? renderCountdown(props.counter) : null}</Text>
   );
 };
 
