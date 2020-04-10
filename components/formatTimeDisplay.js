8fd4ab48b5e78027e2813ac63d05a542a69741a7
Jryke <rykerson@gmail.com>
2020-04-09 21:32:13 -0400

    conditional added to account for null seconds value render

--- components/formatTimeDisplay.js
+++ components/formatTimeDisplay.js
@@ -1,5 +1,6 @@
 export default timeValsObj => {
   if (timeValsObj.minutes != null) {
+    if (timeValsObj.seconds === null) return `${timeValsObj.minutes}:00`
     if (timeValsObj.seconds < 10) return `${timeValsObj.minutes}:0${timeValsObj.seconds}`
     return `${timeValsObj.minutes}:${timeValsObj.seconds}`
   }
