cdf1e02cc900bb1b94dbecb04eb04c9966e1f2a2
Jryke <rykerson@gmail.com>
2020-04-09 20:31:00 -0400

    function refactored into new file for reuse

---
+++ components/formatTimeDisplay.js
@@ -0,0 +1,8 @@
+export default timeValsObj => {
+  if (timeValsObj.minutes != null) {
+    if (timeValsObj.seconds < 10) return `${timeValsObj.minutes}:0${timeValsObj.seconds}`
+    return `${timeValsObj.minutes}:${timeValsObj.seconds}`
+  }
+  if (timeValsObj.seconds < 10) return `0:0${timeValsObj.seconds}`
+  return `0:${timeValsObj.seconds}`
+};
